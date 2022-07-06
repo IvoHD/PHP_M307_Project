e0a014bbf5880895b373575e491deeb6be12c6b1
IvoHD <ivo10ivo@hotmail.com>
2022-07-06 22:14:44 +0200

    html for register and edit

--- app/Views/register.view.php
+++ app/Views/register.view.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
+    <base href="<?= ROOT_URL ?>/">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +9,49 @@
     <title>Neuen Eintrag erfassen</title>
 </head>
 <body>
-    <div class="Container-Main">
-        
-    </div>
+<h1>Registrieren</h1>
+    <form action="" method="POST">
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td>
+                    <input type="text" required>
+                </td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td>
+                    <input type="text" required>
+                </td>
+            </tr>
+            <tr>
+                <td>Telefon:</td>
+                <td>
+                    <input type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>Frucht:</td>
+                <td>
+                    <input type="text" required>
+                </td>
+            </tr>
+            <tr>
+                <td>Mengen-Kategorie:</td>
+                <td>
+                    <input type="text" required>
+                </td>
+            </tr>
+            <tr>
+                <td>Dörr-Status:</td>
+                <td> 
+                    <input type="text" required>
+                </td>
+            </tr>
+        </table>
+        <button type="submit">Editieren</button>
+        <button>Abbrechen</button><br>
+        <button>Zurück</button>
+    </form>
 </body>
 </html>
