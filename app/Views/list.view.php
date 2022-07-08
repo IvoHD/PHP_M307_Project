539b29b4e43f0e5485cf6596fd718b0a67ee5e22
IvoHD <ivo10ivo@hotmail.com>
2022-07-08 10:13:56 +0200

    list.view fix

--- app/Views/list.view.php
+++ app/Views/list.view.php
@@ -6,41 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/app.css">
-    <title>Einträge anzeigen</title>
+    <title>Aufträge anzeigen</title>
 </head>
-<body>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>        
-            <th>Email</th>
-            <th>TelefonNr.</th>
-            <th>Kategorie</th>
-            <th>Dörrungsstatus</th>
-            <th>Tage vergangen</th>
-            <th>Furcht</th>
-        </tr>
-       
-        <?php 
-            foreach($OrderArray as $Order)
-            $OnTimeIcon = OnTimeChecker($Order['category'], $Order['elapseddays'], $['isdried'])
-                echo "
-                    <tr>
-                        <th>{$Order['id']}</th>
-                        <th>{$Order['name']}</th>
-                        <th>{$Order['email']}</th>
-                        <th>{$Order['tel']}</th>
-                        <th>{$Order['category']}</th>
-                        <th>{$Order['isdried']}</th>
-                        <th>{$Order['elapseddays']}</th>
-                        <th>{$Order['fruitid']}</th>
-                        <th>{
-                        <th><a href=\"/input?id={$Order['id']}\"><button class=\"\">Editieren</button></a></th>
-                    </tr>
-                ";
-        ?>
-    </table>
+<body>  
     <script src="public/js/app.js"></script>
-    </div>
 </body>
 </html>
